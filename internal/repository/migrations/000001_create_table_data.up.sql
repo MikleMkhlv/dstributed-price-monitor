d842c09f302089571d741d3b0e2129a03a0b1915
Mikhaylov Mikhail <mikhaylovms@yandex.ru>
2026-07-16 20:53:52 +0300

    Добавлена реализация CRUD в БД. Поправлен тип odject_id c INTEGER на VARCHAR(50)

--- internal/repository/migrations/000001_create_table_data.up.sql
+++ internal/repository/migrations/000001_create_table_data.up.sql
@@ -1,6 +1,6 @@
 -- migrations/000001_create_table_data.up.sql
 CREATE TABLE IF NOT EXISTS data (
     id SERIAL PRIMARY KEY,
-    object_id INTEGER NOT NULL UNIQUE,
+    object_id VARCHAR(50) NOT NULL UNIQUE,
     object_data TEXT NOT NULL
 );
--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -3,10 +3,12 @@ package repository
 import (
 	"context"
 	"dstributed-price-monitor/config"
+	"errors"
 	"fmt"
 	"log"
 	"strings"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -60,14 +62,84 @@ func createConnectionStr(cfg *config.Config) string {
 	return connStr.String()
 }
 
-// func (p *Postgres) Create(ctx context.Context, id string, data []byte) error {
-// }
+func (p *Postgres) Create(ctx context.Context, id string, data []byte) error {
+	query := "INSERT INTO data (object_id, object_data) VALUES ($1, $2)"
+	_, err := p.Pool.Exec(ctx, query, id, data)
+	if err != nil {
+		return fmt.Errorf("repository.Postgres.Create:{%v} error get data: %v", ctx.Value("operId"), err)
+	}
 
-// func (p *Postgres) GetByID(ctx context.Context, id string) ([]byte, error) {
-// }
+	log.Printf("repository.Postgres.Create:{%v} create data in database is successful. id = %s", ctx.Value("operId"), id)
 
-// func (p *Postgres) Update(ctx context.Context, id string, data []byte) ([]byte, error) {
-// }
+	return nil
+}
 
-// func (p *Postgres) Delete(ctx context.Context, id string) error {
-// }
+func (p *Postgres) GetByID(ctx context.Context, id string) ([]byte, error) {
+	query := "SELECT object_data FROM data WHERE object_id = $1"
+	var data []byte
+	if err := p.Pool.QueryRow(ctx, query, id).Scan(&data); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, fmt.Errorf("repository.Postgres.GetByID:{%v} data is not found. id=%s", ctx.Value("operId"), id)
+		}
+		return nil, fmt.Errorf("repository.Postgres.GetByID:{%v}: %v", ctx.Value("operId"), err)
+	}
+	log.Printf("repository.Postgres.GetByID:{%v} data is found. id=%s", ctx.Value("operId"), id)
+	return data, nil
+}
+
+func (p *Postgres) Update(ctx context.Context, id string, data []byte) ([]byte, error) {
+	trz, err := p.Pool.Begin(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("repository.Postgres.Update:{%v} error create transsaction for id=%s. %v", ctx.Value("operId"), id, err)
+	}
+
+	defer func() {
+		if err != nil {
+			trz.Rollback(ctx)
+		}
+	}()
+	tempData := struct {
+		Id     int
+		Obj_id int
+		Data   []byte
+	}{}
+
+	findQuery := "SELECT id, object_id, object_data FROM data WHERE object_id = $1"
+	if err := trz.QueryRow(ctx, findQuery, id).Scan(
+		&tempData.Id,
+		&tempData.Obj_id,
+		&tempData.Data,
+	); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, fmt.Errorf("repository.Postgres.Update:{%v} data not found by id = %s. %v", ctx.Value("operId"), id, err)
+		}
+	}
+	if string(tempData.Data) != string(data) {
+		return nil, fmt.Errorf("repository.Postgres.Update:{%v} The data has not changed by id = %s", ctx.Value("operId"), id)
+	}
+
+	updateQuery := "UPDATE data SET object_data = $1 WHERE object_id = $2"
+
+	_, err = trz.Exec(ctx, updateQuery, data, id)
+	if err != nil {
+		return nil, fmt.Errorf("repository.Postgres.Update:{%v} error update data by id = %s. %v", ctx.Value("operId"), id, err)
+	}
+
+	err = trz.Commit(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("repository.Postgres.Update:{%v} %v", ctx.Value("operId"), err)
+	}
+
+	log.Printf("repository.Postgres.Update:{%v} update data by id = %s is successful", ctx.Value("operId"), id)
+	return tempData.Data, nil
+}
+
+func (p *Postgres) Delete(ctx context.Context, id string) error {
+	query := "DELETE FROM data WHERE object_id = $1"
+	_, err := p.Pool.Exec(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("repository.Postgres.Delete:{%v} error delete data by id = %s. %v", ctx.Value("operId"), id, err)
+	}
+	log.Printf("repository.Postgres.Delete:{%v} update data by id = %s is successful", ctx.Value("operId"), id)
+	return nil
+}
