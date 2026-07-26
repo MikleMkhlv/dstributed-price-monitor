c474fe6b19898c3a44767800f596b5cd41f03968
Mikhaylov Mikhail <mikhaylovms@yandex.ru>
2026-07-26 10:29:09 +0300

    Добавлен nats в композ. Получение настроек nats через yaml. Сделан конструкор для создания соединения с NUTS

--- go.mod
+++ go.mod
@@ -24,12 +24,16 @@ require (
 	github.com/goccy/go-yaml v1.19.2 // indirect
 	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.18.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.4.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-isatty v0.0.23 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nats-io/nats.go v1.52.0 // indirect
+	github.com/nats-io/nkeys v0.4.15 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.4.3 // indirect
 	github.com/quic-go/qpack v0.6.0 // indirect
 	github.com/quic-go/quic-go v0.60.0 // indirect
---
+++ internal/broker/nats.go
@@ -0,0 +1,45 @@
+package broker
+
+import (
+	"dstributed-price-monitor/config"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+type Nats struct {
+	conn *nats.Conn
+}
+
+func NewNats(cfg *config.Config) (*Nats, error) {
+	addr := cfg.Nats.Address
+	if addr == "" {
+		addr = nats.DefaultURL
+	}
+
+	strings.TrimSpace(addr)
+	ns, err := nats.Connect(
+		addr,
+		nats.Timeout(time.Second*time.Duration(cfg.Nats.Timeout)),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error connect to nuts(%s). %v", addr, err)
+	}
+
+	log.Print("broker.Nats.NewNats: connections with nuts is sucessfull:", ns.Status())
+	return &Nats{
+		conn: ns,
+	}, nil
+}
+
+func (n *Nats) Close() error {
+	if n.conn == nil {
+		return fmt.Errorf("The nuts connections had already been broken.")
+	}
+	n.conn.Close()
+	fmt.Print("broker.Nats.NewNats: connections with nuts is close")
+	return nil
+}
